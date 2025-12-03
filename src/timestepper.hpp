ee3fd777d2e16986dc7cefc2c355475caad7322f
Angelo2510 <angelo2510.as@gmail.com>
2025-12-03 18:57:54 +0100

    bis crank nicholson alles gemacht

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -83,6 +83,56 @@ namespace ASC_ode
     }
   };
 
+    class CrankNicolson : public TimeStepper
+  {
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
+    std::shared_ptr<ConstantFunction> m_fold;   // f(y_n)
+
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs)
+      : TimeStepper(rhs),
+        m_tau(std::make_shared<Parameter>(0.0))
+    {
+      // y_n und f(y_n) als konstante Funktionen
+      m_yold = std::make_shared<ConstantFunction>(rhs->dimX());
+      m_fold = std::make_shared<ConstantFunction>(rhs->dimF());
+
+      // y_new
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+
+      // Funktionsgleichung:
+      // G(y_new) = y_new - y_old - 0.5*tau*( f(y_old) + f(y_new) )
+      //
+      // 0.5*tau * f(y_new)
+      //auto half_tau = 0.5 * m_tau;
+      auto term_new = 0.5 * (m_tau * m_rhs);
+
+      // 0.5*tau * f(y_old)  -> hier benutzen wir m_fold
+      auto term_old = 0.5 * (m_tau * m_fold);
+
+      m_equ = ynew - m_yold - term_new - term_old;
+    }
+
+    void doStep(double tau, VectorView<double> y) override
+    {
+      // y_n speichern
+      m_yold->set(y);
+
+      // f(y_n) in m_fold speichern
+      Vector<> tmp_f(m_rhs->dimF());
+      m_rhs->evaluate(y, tmp_f);
+      m_fold->set(tmp_f);
+
+      m_tau->set(tau);
+
+      // Newton löst G(y_{n+1}) = 0, Ausgangswert = y_n
+      NewtonSolver(m_equ, y);
+    }
+  };
+
+
 
   
 
