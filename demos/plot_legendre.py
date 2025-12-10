54015b8cb1c48fba76e93e37cad2443455c5faed
CarlosDuenasLopez <caaldulo@gmail.com>
2025-12-10 18:26:11 +0100

    legendre plot

---
+++ demos/plot_legendre.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load data (skip the header row)
+data = np.loadtxt("build/legendre_data.txt", skiprows=1)
+
+x = data[:, 0]
+P = data[:, 1:]  # columns P0..P5
+
+degrees = P.shape[1]  # should be 6 (0..5)
+
+plt.figure()
+
+for n in range(degrees):
+    plt.plot(x, P[:, n], label=f"P{n}(x)")
+
+plt.xlabel("x")
+plt.ylabel("P_n(x)")
+plt.title("Legendre Polynomials P0..P5")
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
